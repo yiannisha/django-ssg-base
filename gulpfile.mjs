f6fd32cb767d13e62ddeafa942c41e517b347039
yiannisha <hadjiyiannis.3@gmail.com>
2022-10-21 17:33:48 +0300

    CSS file processing

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,8 +1,20 @@
+import fs from 'fs';
 import gulp from 'gulp';
 import imagemin from 'gulp-imagemin';
 import webp from 'gulp-webp';
 import replace from 'gulp-replace';
+// import uglify from 'gulp-uglify';
+import rev from 'gulp-rev';
+import cleanCSS from 'gulp-clean-css';
+import htmlmin from 'gulp-htmlmin';
+import concatCSS from 'gulp-concat-css';
 import { deleteAsync } from 'del';
+
+const STATIC_DIR = './dist/static';
+const IMG_DIR = STATIC_DIR+ '/img';
+const CSS_DIR = STATIC_DIR + '/css';
+
+// --- IMAGES ---
 
 // compress JPEG, JPG, PNG and GIF images 
 gulp.task('imagemin', function () {
@@ -38,5 +50,61 @@ gulp.task('replace-image-paths', function () {
 
 gulp.task('process-images', gulp.series('imagemin', 'webp', 'delete-images', 'replace-image-paths'));
 
+// --- CSS Files ---
+
+// Minify CSS files
+gulp.task('minify-css', function () {
+    return gulp.src(`${CSS_DIR}/*.css`)
+           .pipe(cleanCSS())
+           .pipe(gulp.dest(CSS_DIR));
+});
+
+// Concatenate CSS files
+gulp.task('concat-css', function () {
+    return gulp.src('./dist/static/css/*.css')
+           .pipe(concatCSS('main.css'))
+           .pipe(gulp.dest(CSS_DIR));
+});
+
+// Add hash to main CSS file name
+gulp.task('rev-css', function () {
+    return gulp.src(`${CSS_DIR}/main.css`)
+           .pipe(rev())
+           .pipe(gulp.dest(CSS_DIR))
+           .pipe(rev.manifest())
+           .pipe(gulp.dest('./config'));
+});
+
+// Remove extra CSS files from dist
+gulp.task('remove-extra-css', function () {
+    return deleteAsync([`${CSS_DIR}/*.css`, `!${CSS_DIR}/main-*.css`]);
+});
+
+// Replace css CSS file paths in HTML files
+gulp.task('replace-css-paths', async function () {
+    throw new Error ('Not yet implemented');
+
+    fs.readFile('./config/rev-manifest.json', 'utf-8', (err, jsonString) => {
+        if (err) {
+            throw err;
+        }
+        const mainCssFilename = JSON.parse(jsonString)['main.css'];
+        return gulp.src('./dist/*html')
+        .pipe(replace(/about|home\.css/g, mainCssFilename))
+        .pipe(gulp.dest('./dist'));
+    });
+});
+
+gulp.task('process-css', gulp.series('minify-css', 'concat-css', 'rev-css', 'remove-extra-css'));
+
+// --- HTML Files ---
+
+// Minify HTML files
+gulp.task('htmlmin', function () {
+    return gulp.src('./dist/*.html')
+           .pipe(htmlmin({ collapseWhitespace: true }))
+           .pipe(gulp.dest('./dist'));
+});
+
 // build task
-gulp.task('build', gulp.series('process-images'));
+gulp.task('build', gulp.series('process-images', gulp.parallel('htmlmin', 'process-css')));
