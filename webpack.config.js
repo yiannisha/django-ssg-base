f21258fde2756256a3edf64ce6612de93469f28b
yiannisha <hadjiyiannis.3@gmail.com>
2022-11-18 08:18:12 +0200

    Automatically get app entry points

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,45 @@
 const path = require('path');
+const fs = require('fs');
+
+/**
+ *  
+ * @returns {list[str]} entryPoints: list of app js bundle entry points 
+ */
+ function getEntryPoints () {
+    // get all directories that are apps (search for a templates folder inside)
+    var entryPoints = {};
+    
+    fs.readdirSync(__dirname).forEach((folder) => {
+        if (folder === path.basename(__dirname)) {
+            return;
+        }
+        
+        if (fs.lstatSync(folder).isDirectory()) {
+            const isApp = fs.readdirSync(folder).find((subfolder) => subfolder === "templates");
+            if (isApp) {
+                const staticPath = path.join(folder, "static");
+                const hasJS = fs.readdirSync(staticPath).find((sub) => sub === "js");
+                if (hasJS) {
+                    console.log(folder);
+                    entryPoints[folder] = "./" + path.join(staticPath, "js", `${folder}.js`);
+                }
+            }
+        }
+    });
+
+    return entryPoints;
+}
+
 
 module.exports = (env) => ({
     mode: env.mode,
     entry: {
-        home: './dist/static/js/home.js',
-        aboutus: './dist/static/js/aboutus.js',
         transitions: './dist/static/js/transitions.js',
+        ...getEntryPoints()
     },
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, 'dist/static/js'),
-        // doesn't work for some reason
-        // clean: true
+        clean: true
     },
 });
